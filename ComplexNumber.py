dc440e1a0008e65e9c3e00353cbc51acb4755663
unknown <qamar.abbas14@hotmail.com>
2020-11-08 21:14:19 +0500

    Multiplication of numbers enabled

--- ComplexNumber.py
+++ ComplexNumber.py
@@ -1,5 +1,7 @@
+from enum import auto
 
 class ComplexNumber(object):
+    description="A Complex Number Class"
     def __init__(self,real,img):
         self.img_part=img
         self.real_part=real
@@ -40,18 +42,45 @@ class ComplexNumber(object):
             obj.img_part=self.img_part-value.img_part
             return obj
 
+    def __mul__(self, value):
+        if isinstance(value, int):
+            obj= ComplexNumber(0,0)
+            obj.real_part=self.real_part * value
+            obj.img_part=self.img_part * value
+            return obj
+        if isinstance(value, ComplexNumber):
+            obj= ComplexNumber(0,0)
+            a = value.real_part
+            b = value.img_part
+            x = self.real_part
+            y = self.img_part
+
+            fx_real  = a*x - b*y
+            fx_img = b*x + a*y   
+            obj.img_part = fx_img
+            obj.real_part= fx_real
+
+            return obj
+
 
 
 if __name__=='__main__':
-    num1=ComplexNumber(-2,7)
-    num2=ComplexNumber(3,4)
+    num1=ComplexNumber(2,-7)
+    num2=ComplexNumber(-3,-4)
     num3=num1+2
     num4=num1+num3
     num5=num1-num2
-    print(num3)
+
+    print("num1 =",num1)
+    print("num2 =",num2)
+    print("num3 = num1+2 = ",num3)
     print(repr(num3))
-    print(num1)
-    print(num4)
-    print(num5)
-    print(type(num4))
+    print("num4=num1+num3=",num4)
+    print("num5=num1-num2=",num5)
+    print("num6=num5 * 10",num5*10)
+    print("num1 * num2",num1*num2)
+    
+    print(num1.__dict__)
+    print(num1.description)
+    
 
